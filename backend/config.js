878647ff7c50d4a2a9069305aa85a7f9b0882fef
yifan996 <143735441+yifan996@users.noreply.github.com>
2026-02-21 16:10:24 +0800

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ module.exports = {
     // 服务器端口
     PORT: 3000,
     // MongoDB 连接地址（本地默认）
-    MONGODB_URI: 'process.env.MONGODB_URI',
+    MONGODB_URI: process.env.MONGODB_URI,
     // 远程 MongoDB 示例（替换为你的 Atlas 连接字符串）
     // MONGODB_URI: 'mongodb+srv://<你的用户名>:<你的密码>@cluster0.mongodb.net/travelAssistant'
     COZE_API_TOKEN:'pat_t9biOkrwI3D6WAjOrk1NswtX5ZDaj2kDtJpm2urcW1EtxT3zRLWpscHwIN7cNMuH',
